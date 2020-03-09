8190259f240565f39fe937c672549172d5532b56
Matheus Neves <matheus.neves@prodigious.com>
2020-03-09 14:25:28 -0300

    add styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { Container } from './styles';
 
 export default function Main() {
   return (
-    <View>
+    <Container>
       <Text>Hello World!</Text>
-    </View>
+    </Container>
   );
 }
 
